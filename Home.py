263df813cf8e3f52c23b840f2faa161e13ebb89e
Bilal <bilal.norath.dev@gmail.com>
2023-08-21 10:28:53 +0200

    New commit to fix deployment issue on Streamlit

--- Home.py
+++ Home.py
@@ -16,15 +16,15 @@ Welcome to my site where we discuss all things EMV. \n
 
 This is a work in progress so please bear with me. I will slowly add more 
 functionality over time. Whilst every effort has been made to use the current
- versions of the various EMV tags, there is no guarantee that I wil be able to 
- update the tags timeously as and when they change. For this reason, please 
- ensure that you verify that the checklists on the site is in line with the 
- latest technical specifications, especially if you are using this site to 
- support your EMV delivery or testing. I cannot be held liable for any errors 
- or outdated specifications that may be present on this site. \n
+versions of the various EMV tags, there is no guarantee that I wil be able to 
+update the tags timeously as and when they change. For this reason, please 
+ensure that you verify that the checklists on the site is in line with the 
+latest technical specifications, especially if you are using this site to 
+support your EMV delivery or testing. I cannot be held liable for any errors 
+or outdated specifications that may be present on this site. \n
 
 If you are on a web browser on a computer/laptop, you will see the translators
- listed below appearing on separate pages on the left hand menu.\n
+listed below appearing on separate pages on the left hand menu.\n
  
 If you are accessing the site using a web browser on a mobile device, please
 click on the > symbol on the upper left hand corner of the screen to access the 
